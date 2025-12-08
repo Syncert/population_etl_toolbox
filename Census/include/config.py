b474a61123c3638cdd433b5a94bb32c4f128b3ea
syncert <syncert@gmail.com>
2025-12-08 16:46:35 -0600

    Added some values to config for interesting tables

--- Census/include/config.py
+++ Census/include/config.py
@@ -13,6 +13,7 @@ class AcsConfig(BaseModel):
     datasets: List[str] = ["acs1", "acs5"]
     # curated table IDs (can expand later)
     curated_tables: List[str] = [
+        #https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fwww2.census.gov%2Fprograms-surveys%2Facs%2Ftech_docs%2Ftable_shells%2F2024%2FACS2024_Supplemental_Table_Shells.xlsx&wdOrigin=BROWSELINK
         "B01001",  # Sex by age
         "B01003",  # Total population
         "B02001",  # Race
@@ -21,8 +22,18 @@ class AcsConfig(BaseModel):
         "B19001",  # Household income dist
         "B19013",  # Median household income
         "B19083",  # Gini inequality index
-        "B25003",  # Tenure
+        "B24114",  # Detailed Occupation for the Civilian Employed Population 16 Years and Over
+        "B24134",  # Detailed Industry for the Civilian Employed Population 16 Years and Over
+        "B25003",  # Tenure (Housing Units, Owner or Renter occupied)
+        "B25075",  # House Value of owner-occupied housing units
+        "B25104",  # Monthly Housing Costs
         "B27010",  # Health insurance by age
+        "C24050",  # Industry by Occupation for the Civilian Employed Population 16 Years and Over
+        "K201903", # Family Income in the Past 12 Months
+        "K201501", # Educational Attainment of the Population 25 years and over
+        "K202403", # Industry for the Civilian Employed Population 16 Years and Over
+        "K202507"  # Gross Rent
+
     ]
     # geo levels we ingest
     geo_levels: List[str] = ["us", "state", "county"]
