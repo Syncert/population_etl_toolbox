0af931f13cac4439954bd4a9a3b1b56ef2e2e2b2
syncert <syncert@gmail.com>
2025-12-09 19:41:33 -0600

    Added additional census codes. Moving onto testing airflow now.

--- Census/include/config.py
+++ Census/include/config.py
@@ -13,7 +13,7 @@ class AcsConfig(BaseModel):
     datasets: List[str] = ["acs1", "acs5"]
     # curated table IDs (can expand later)
     curated_tables: List[str] = [
-        #https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fwww2.census.gov%2Fprograms-surveys%2Facs%2Ftech_docs%2Ftable_shells%2F2024%2FACS2024_Supplemental_Table_Shells.xlsx&wdOrigin=BROWSELINK
+        #https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fwww2.census.gov%2Fprograms-surveys%2Facs%2Fsummary_file%2F2024%2Ftable-based-SF%2Fdocumentation%2FACS2024_Table_Shells.xlsx&wdOrigin=BROWSELINK
         "B01001",  # Sex by age
         "B01003",  # Total population
         "B02001",  # Race
@@ -22,18 +22,26 @@ class AcsConfig(BaseModel):
         "B19001",  # Household income dist
         "B19013",  # Median household income
         "B19083",  # Gini inequality index
+        "B24040",  # Sex by Industry for the Full-Time, Year-Round Civilian Employed Population 16 Years and Over
+        "B24010",  # Sex by Occupation for the Civilian Employed Population 16 Years and Over
         "B24114",  # Detailed Occupation for the Civilian Employed Population 16 Years and Over
         "B24134",  # Detailed Industry for the Civilian Employed Population 16 Years and Over
         "B25003",  # Tenure (Housing Units, Owner or Renter occupied)
         "B25075",  # House Value of owner-occupied housing units
         "B25104",  # Monthly Housing Costs
+        "B25108",  # Aggregate Value (Dollars) by Year Structure Built
         "B27010",  # Health insurance by age
         "C24050",  # Industry by Occupation for the Civilian Employed Population 16 Years and Over
-        "K201903", # Family Income in the Past 12 Months
+        "C24010C", # Sex by Occupation for the Civilian Employed Population 16 Years and Over (American Indian and Alaska Native Alone)
+        "K200104", # Population by Age
+        "K200801", # Means of transportation to work
+        "K201701", # Poverty Status in the Past 12 Months by Age
+        "K201801", # Disability Status by Age
+        "K201903", # Family Income in the Past 12 Months (in 2024 Inflation-Adjusted Dollars)
         "K201501", # Educational Attainment of the Population 25 years and over
+        "K202201", # Receipt of Food Stamps/SNAP in the Past 12 Months by Presence of Children Under 18 Years for Households
         "K202403", # Industry for the Civilian Employed Population 16 Years and Over
         "K202507"  # Gross Rent
-
     ]
     # geo levels we ingest
     geo_levels: List[str] = ["us", "state", "county"]
