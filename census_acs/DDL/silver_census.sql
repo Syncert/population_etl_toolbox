d3cfd39a0e7acd35ffd594953da63c4dfb97548e
Nicholas Kreuziger <syncert@gmail.com>
2026-02-22 19:40:23 -0600

    feat: Add index on source_system and estimate_year for improved query performance

--- census_acs/DDL/silver_census.sql
+++ census_acs/DDL/silver_census.sql
@@ -31,6 +31,7 @@ CREATE INDEX IF NOT EXISTS idx_fact_demo_geo_sk ON silver_census.fact_demographi
 CREATE INDEX IF NOT EXISTS idx_fact_demo_dataset ON silver_census.fact_demographics(dataset);
 CREATE INDEX IF NOT EXISTS idx_fact_demo_table_id ON silver_census.fact_demographics(table_id);
 CREATE INDEX IF NOT EXISTS idx_fact_demo_upsert_key ON silver_census.fact_demographics(dataset, table_id, variable_code, geo_id, estimate_year);
+CREATE INDEX IF NOT EXISTS idx_fact_demo_source_year ON silver_census.fact_demographics(source_system, estimate_year);
 
 -- Autovacuum for this high-update table
 ALTER TABLE silver_census.fact_demographics SET (
